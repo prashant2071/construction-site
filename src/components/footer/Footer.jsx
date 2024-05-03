4de3d75121a91a2a5c8c1d98bf9fed82c871b17d
prashanta <pbt9840@gmail.com>
2024-05-03 18:16:50 +0545

    feat:footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { CiTwitter } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import "./footer.css";
+import frame from "./Frame.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="container">
+        <div className="top-content">
+          <img src={frame} alt="logo" />
+          <p>
+            Making a world a better place through constructing elegant
+            hierarchies
+          </p>
+          <div className="logo">
+            <FaFacebook />
+            <CiTwitter />
+            <FaInstagram />
+          </div>
+        </div>
+        <div className="footer-content">
+          <h3>Explore</h3>
+          <ul className="list-item">
+            <li>About</li>
+            <li>Blog</li>
+            <li>Press</li>
+            <li>Partner</li>
+          </ul>
+        </div>
+        <div className="footer-content">
+          <h3>Quick Links</h3>
+          <ul className="list-item">
+            <li>About</li>
+            <li>Blog</li>
+            <li>Press</li>
+            <li>Partner</li>
+          </ul>
+        </div>
+        <div className="footer-content">
+          <h3>Stay in touch</h3>
+          <div className="content">
+            <h3>Call us</h3>
+            <p>9856968752,9858958789</p>
+          </div>
+          <div className="content">
+            <h3>Send Us Email</h3>
+            <p>danghoang87hl@gmail.com</p>
+          </div>
 
-export default Footer
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Footer from '../components/footer/Footer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Footer></Footer>
+    </div>
   )
 }
 
