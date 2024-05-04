b828ba26042f4f9dbcd4aaa63230d4ddc8dfe892
samrat malla <samratmalla48@gmail.com>
2024-05-04 15:35:56 +1000

    classname modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from '../components/footer/Footer'
+import Navbar from '../components/navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Footer></Footer>
     </div>
   )
