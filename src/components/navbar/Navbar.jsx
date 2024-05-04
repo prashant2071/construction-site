98f25d2b6d00f3e69fa7e636880ccc9379db7b0b
prashanta <pbt9840@gmail.com>
2024-05-04 11:29:05 +0545

    css issue resolved

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,42 +1,47 @@
-import React from 'react'
-import '../navbar/navbar.css'
-import logo from './logo.png'
+import React from "react";
+import "../navbar/navbar.css";
+import logo from "./logo.png";
 
 const Navbar = () => {
   return (
     <div>
-  
-
-<header className='container'>
-      
-      <div className='logo' >
-      <img  src={logo} alt="logo" />
-      </div>
-      <div className='navbar'>
-      <nav>
-        <ul className='nav-items'>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Our Projects</a></li>
-          <li><a href="#">Partners</a></li>
-          <li><a href="#">Commitments</a></li>
-          <li><a href="#">Media Room</a></li>
-        </ul>
-      </nav>
-      </div>
-      <div>
-      <button class="btn">Contact Us</button>
-      </div>
-    </header>
-
-
-
-
-       
-      
+      <header className="container">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="navbar">
+          <nav>
+            <ul className="nav-items">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Our Projects</a>
+              </li>
+              <li>
+                <a href="#">Partners</a>
+              </li>
+              <li>
+                <a href="#">Commitments</a>
+              </li>
+              <li>
+                <a href="#">Media Room</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <button class="btn">Contact Us</button>
+        </div>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from '../components/footer/Footer'
+import Navbar from '../components/navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
-      <Footer></Footer>
+      <Navbar/>
+      <Footer/>
     </div>
   )
 }
