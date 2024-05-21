72af1491a625634c0b926c682d449ac1161ecef2
prashanta <pbt9840@gmail.com>
2024-05-21 23:57:01 +0545

    feat:content page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
-import About from "./pages/About";
+import About from "./pages/about/About";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Card from "./components/card/Card";
 import NoPageFound from "./pages/NoPageFound";
+import Services from "./pages/service/Services";
 function App() {
   return (
     <>
@@ -13,6 +14,8 @@ function App() {
         <Routes>
           <Route path="" element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+
           <Route path="card" element={<Card/>} />
           <Route path="*" element={<NoPageFound/>} />
 
